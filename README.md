87057252999436987a621fe12381100d44465a73
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-09-29 21:54:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# FSW-DATE-HANDLER
+
+Biblioteca para tratamento de data/hora com formato utilizado pelos sistemas Senior G5 em projetos JS. 
+
---
+++ index.js
@@ -0,0 +1,13 @@
+function helloFswLib() {
+    return 'Hello from lib!';
+}
+
+function actualDateOnG5Format() {
+
+}
+
+function actualHourOnG5Format() {
+
+}
+
+module.exports = { helloFswLib, actualDateOnG5Format, actualHourOnG5Format }
